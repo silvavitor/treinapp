6b4482a1e58d4cf6a6ac19b7071b57f0a9ccf1b0
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2024-01-06 15:47:00 -0300

    feat: add cors to front

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: '*',
+    allowedHeaders: '*',
+    maxAge: 10,
+  });
   await app.listen(3000);
 }
 bootstrap();
